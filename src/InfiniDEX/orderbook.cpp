86e053f08f291a8e4effbc16687ffb8ec4ee2886
unknown <infinex.coin@gmail.com>
2018-03-09 16:43:34 +0800

    InfiniDEX

--- src/InfiniDEX/orderbook.cpp
+++ src/InfiniDEX/orderbook.cpp
@@ -17,30 +17,48 @@ extern COrderBookManager orderBookManager;
 
 void COrderBookManager::AddToAsk(int CoinID, uint64_t OrderPrice, uint64_t Quantity, uint64_t Amount)
 {
-    std::map<int, PriceOrderBook>::iterator it = orderBidBook.find(CoinID);
-    if(it != orderBidBook.end())
-    {
-        PriceOrderBook::iterator it2 = it->second.find(OrderPrice);
-        if(it2 != it->second.end())
-        {
-        }
-        else
-        {            
-        }
-    }
+    std::map<int, PriceOrderBook>::iterator it = orderAskBook.find(CoinID);
+	if (it != orderAskBook.end())
+	{
+		PriceOrderBook::iterator it2 = it->second.find(OrderPrice);
+		if (it2 != it->second.end())
+		{
+			it2->second.nQuantity += Quantity;
+			it2->second.nAmount = OrderPrice * it2->second.nQuantity;
+		}
+		else
+		{
+			orderAskBook[CoinID][OrderPrice] = COrderBook(OrderPrice, Quantity, OrderPrice*Quantity, 1234);
+		}
+	}
+	else
+	{
+		PriceOrderBook NewCoinID;
+		NewCoinID.insert(std::make_pair(OrderPrice, COrderBook(OrderPrice, Quantity, OrderPrice*Quantity, 1234)));
+		orderAskBook.insert(std::make_pair(CoinID, NewCoinID));
+	}
 }
 
 void COrderBookManager::AddToBid(int CoinID, uint64_t OrderPrice, uint64_t Quantity, uint64_t Amount)
 {
-    std::map<int, PriceOrderBook>::iterator it = orderAskBook.find(CoinID);
-    if(it != orderAskBook.end())
-    {
-        PriceOrderBook::iterator it2 = it->second.find(OrderPrice);
-        if(it2 != it->second.end())
-        {            
-        }
-        else
-        {            
-        }
-    }
+    std::map<int, PriceOrderBook>::iterator it = orderBidBook.find(CoinID);
+	if (it != orderBidBook.end())
+	{
+		PriceOrderBook::iterator it2 = it->second.find(OrderPrice);
+		if (it2 != it->second.end())
+		{
+			it2->second.nQuantity += Quantity;
+			it2->second.nAmount = OrderPrice * it2->second.nQuantity;
+		}
+		else
+		{
+			orderBidBook[CoinID][OrderPrice] = COrderBook(OrderPrice, Quantity, OrderPrice*Quantity, 1234);
+		}
+	}
+	else
+	{
+		PriceOrderBook NewCoinID;
+		NewCoinID.insert(std::make_pair(OrderPrice, COrderBook(OrderPrice, Quantity, OrderPrice*Quantity, 1234)));
+		orderBidBook.insert(std::make_pair(CoinID, NewCoinID));
+	}
 }
