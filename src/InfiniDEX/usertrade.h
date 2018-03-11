88adaf2c93b60c2b22df1ba2af5a9483569037d2
Infinex <33971094+InfinexOfficial@users.noreply.github.com>
2018-03-11 19:50:33 +0800

    InfiniDEX

--- src/InfiniDEX/usertrade.h
+++ src/InfiniDEX/usertrade.h
@@ -12,6 +12,81 @@
 class CUserTrade;
 class CUserTradeManager;
 
+class CUserTrade
+{
+private:
+	std::vector<unsigned char> vchSig;
 
+public:
+	int nTradePairID;
+    uint64_t nPrice;
+    uint64_t nQuantity;
+    uint64_t nAmount;    
+	std::string nUserPubKey;
+	uint64_t nTimeSubmit;
+	int64_t nBalanceQty;
+    int64_t nBalanceAmount;
+    uint64_t nLastUpdate;
+
+	CUserTrade(int nTradePairID, uint64_t nPrice, uint64_t nQuantity, uint64_t nAmount, std::string nUserPubKey, uint64_t nTimeSubmit, int64_t nBalanceQty, int64_t nBalanceAmount, uint64_t nLastUpdate) :
+		nTradePairID(nTradePairID),
+		nPrice(nPrice),
+		nQuantity(nQuantity),
+		nAmount(nAmount),
+        nUserPubKey(nUserPubKey),
+        nTimeSubmit(nTimeSubmit),
+        nBalanceQty(nBalanceQty),
+        nBalanceAmount(nBalanceAmount),
+        nLastUpdate(nLastUpdate)
+	{}
+
+	CUserTrade() :
+		nTradePairID(0),
+		nPrice(0),
+		nQuantity(0),
+		nAmount(0),
+        nUserPubKey(""),
+        nTimeSubmit(0),
+        nBalanceQty(0),
+        nBalanceAmount(0),
+        nLastUpdate(0)
+	{}
+
+
+	ADD_SERIALIZE_METHODS;
+
+	template <typename Stream, typename Operation>
+	inline void SerializationOp(Stream& s, Operation ser_action, int nType, int nVersion) {
+		READWRITE(nTradePairID);
+		READWRITE(nPrice);
+		READWRITE(nQuantity);
+		READWRITE(nAmount);
+        READWRITE(nUserPubKey);
+		READWRITE(nTimeSubmit);
+		READWRITE(nBalanceQty);
+		READWRITE(nBalanceAmount);
+        READWRITE(nLastUpdate);
+		READWRITE(vchSig);
+	}
+
+	uint256 GetHash() const
+	{
+		CHashWriter ss(SER_GETHASH, PROTOCOL_VERSION);
+		ss << nTradePairID;
+		ss << nPrice;
+		ss << nQuantity;
+		ss << nAmount;
+        ss << nUserPubKey;
+		ss << nTimeSubmit;
+		ss << nBalanceQty;
+		ss << nBalanceAmount;
+		ss << nLastUpdate;
+		return ss.GetHash();
+	}
+
+	bool Sign(std::string strSignKey);
+	bool CheckSignature();
+	void Relay(CConnman& connman);
+};
 
 #endif
