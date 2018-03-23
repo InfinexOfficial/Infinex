6e3ba52a2fab7a8e7dd6de13d1793abff616ccd6
unknown <infinex.coin@gmail.com>
2018-03-23 10:41:46 +0800

    InfiniDEX

--- src/InfiniDEX/noderole.cpp
+++ src/InfiniDEX/noderole.cpp
@@ -3,3 +3,9 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "stdafx.h"
+#include "noderole.h"
+
+std::map<int, pairIPPubKeyNodeRole> mapTradePairGlobalRoleByIPPubKey;
+std::map<int, pairNodeRoleByRole> mapTradePairGlobalRoleByRole;
+std::map<int, std::vector<CNodeRole*>> mapTradePairNodeRole;
+CNodeRoleManager nodeRoleManager;
--- src/InfiniDEX/noderole.h
+++ src/InfiniDEX/noderole.h
@@ -12,17 +12,58 @@
 class CNodeRole;
 class CNodeRoleManager;
 
+typedef std::pair <std::string, std::string> pairIPPubkey;
+typedef std::pair <pairIPPubkey, std::vector<CNodeRole*>> pairIPPubKeyNodeRole;
+typedef std::pair <infinidex_node_role_enum, std::vector<CNodeRole*>> pairNodeRoleByRole;
+extern std::map<int, pairIPPubKeyNodeRole> mapTradePairGlobalRoleByIPPubKey;
+extern std::map<int, pairNodeRoleByRole> mapTradePairGlobalRoleByRole;
+extern std::map<int, std::vector<CNodeRole*>> mapTradePairNodeRole;
+extern CNodeRoleManager nodeRoleManager;
+
 enum infinidex_node_role_enum {
-	INFINIDEX_NOTHING=0,
-    INFINIDEX_PRICE_BROADCAST=1,
-    INFINIDEX_HISTORY_PROVIDER=2,
-    INFINIDEX_BALANCE_INFO=3,
-    INFINIDEX_TRUSTED_NODE=4,
-    INFINIDEX_VERIFICATOR=5,
-    INFINIDEX_BACKUP_NODE=6
+	INFINIDEX_NOTHING = 0,
+	INFINIDEX_USER = 1,
+	INFINIDEX_ORDERBOOK_BROADCAST = 2,
+	INFINIDEX_HISTORY_PROVIDER = 3,
+	INFINIDEX_CHARTDATA_PROVIDER = 4,
+	INFINIDEX_BALANCE_INFO = 5,
+	INFINIDEX_TRUSTED_NODE = 6,
+	INFINIDEX_VERIFICATOR = 7,
+	INFINIDEX_BACKUP_NODE = 8
 };
 
 extern std::map<int, std::vector<infinidex_node_role_enum>> mapCoinIDNodeRoles;
 extern CNodeRoleManager nodeRoleManager;
+
+class CNodeRole
+{
+public:
+	int NodeRoleID;
+	int TradePairID;
+	infinidex_node_role_enum NodeRole;
+	std::string NodeIP;
+	std::string NodePubKey;
+	bool IsValid;
+	int ToReplaceNodeRoleID;
+	uint64_t AppointTime;
+	std::string AuthorisedSignature;
+
+	CNodeRole(int NodeRoleID, int TradePairID, infinidex_node_role_enum NodeRole, std::string NodeIP, std::string NodePubKey, bool IsValid, int ToReplaceNodeRoleID, uint64_t AppointTime, std::string AuthorisedSignature):
+		NodeRoleID(NodeRoleID),
+		TradePairID(TradePairID),
+		NodeRole(NodeRole),
+		NodeIP(NodeIP),
+		NodePubKey(NodePubKey),
+		IsValid(IsValid),
+		ToReplaceNodeRoleID(ToReplaceNodeRoleID),
+		AppointTime(AppointTime),
+		AuthorisedSignature(AuthorisedSignature)
+	{}
+};
+
+class CNodeRoleManager
+{
+	
+};
 
 #endif
